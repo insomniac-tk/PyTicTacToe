2789fc9c5268d68cd0b074597950ac571b019ee0
insomniac-tk <shirleyhouse20@gmail.com>
2019-10-07 23:35:30 +0530

    Added functions.

--- tictactoe.py
+++ tictactoe.py
@@ -1,46 +1,81 @@
-from os import system,name
-from time import sleep 
+'''
+	Author : Tejas Khanna
 
-# Some globals
-players = {}
-players['p1_move'] = 'X'
-players['p2_move'] = 'O'
+'''
+from random import randint
+import time
+import sys
 
-GameOver = False
-initial_board = [[1,2,3],[4,5,6],[7,8,9]]
+def game_header():
+	print("### PyTicTacToe ###")
+	print("Developed By Tejas Khanna | Version 0.1")
+	print(" This is the python implementation of the famous two player paper and pencil strategy game.")
+	print("Feel free to use this code and modify it as you please :D ")
 
-def board(l=initial_board):
-    global GameOver
-    for i in l:
-        if i == ['X']*3 or i == ['O']*3:
-            GameOver = True
-    return GameOver
+def delay_print(s):
+    for c in s:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.25)
 
-def hr():
-    print('=====================================')
+def display_numpad():
+	print(" - "*10)
+	print("\tPyTicTacToe")
+	print(" - "*10)
+	for i in range(9,0,-3):
+		k = i 
+		print("\t| ({}) ({}) ({}) |".format(k-2,k-1,k))
+		print()
+	print(" - "*10)
+
+def display(board):
+	print(" - "*10)
+	print("\tPyTicTacToe")
+	print(" - "*10)
+	for i in range(9,0,-3):
+		k = i 
+		print("\t| ({}) ({}) ({}) |".format(board[k-2],board[k-1],board[k]))
+		print()
+	print(" - "*10)
+
+def place_mark(mark,position,board):
+	board[position] = mark
+
+def input_players():
+	p1 = input("Enter your name player 1: ")
+	p2 = input("Enter your name player 2: ")
+	print("Welcome on board {} and {}".format(p1,p2))
+	tmp = ''
+	while not(tmp=='X' or tmp=='O'):
+		tmp = input("Choose your mark player1(X or O) => ")
+	p1_mark = tmp
+	p2_mark = 'X' if p1_mark == 'O' else 'O'
+	print("{}: {},{} : {}".format(p1,p1_mark,p2,p2_mark))
+	
+	print("Now the computer will randomly pick who starts first ")
+	print("Computer is deciding",end='')
+	delay_print('.'*10)
+	print()
+	time.sleep(5)
+	coin_flip = randint(0,1)
+	first_chance = coin_flip 
+
+	if coin_flip == 1:
+		print('{} will start'.format(p1))
+	else:
+		print('{} will start'.format(p2))
+
+	print("All the best {} & {} for the game...!".format(p1,p2))
 
 
-def startup():
-    system('clear')
-    print("\t############# PyTicTactToe v1.0 #############\n\t --------------------------------------")
-    p1 = input('Enter Player 1 Name - ')
-    p2 = input('Enter Player 2 Name - ')
-    players['p1'] = p1
-    players['p2'] = p2
-    print('Welcome  {} and {}! May the best player win!'.format(players['p1'],players['p2']))
-    hr()
-    print('{} moves first'.format(players['p1']))
-    print('{} = X and {} = O'.format(players['p1'],players['p2']))
-    print("Let's begin")
-    board()
-    #game()
+def rules():
+	b = ['']*10	
+	#print(len(b))
+	display_numpad()
+	print('You have to use the numpad on your keyboard to pick the position\nwhere you would like to place your mark(X or O).')
+	print('This is what an empty board looks like')
+	display(b)
+	print("Say, I wish to place an X on position 9, this is what the board will look like after that")
+	place_mark('X',9,b)	
+	display(b)
 
-
-def game():
-    while GameOver == False:
-        pos  = input('{} , enter the postion where you want to put an X >> '.format(players['p1']))
-          
-l = [[1,2,3],[4,5,6],['X','X','X']]
-print(board(l))
-print(GameOver)
-#startup()
